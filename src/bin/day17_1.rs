37a02c79683fa33eede1f2ec4959b31e7e7f79d2
Eugine Kosenko <eugine.kosenko@gmail.com>
2024-12-01 17:32:22 +0200

    Day 18, part 1

--- src/bin/day17_1.rs
+++ src/bin/day17_1.rs
@@ -1,95 +1,15 @@
 use std::{fs, env, io::{self, BufRead}};
 use grid::Grid;
-use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
-use std::cmp;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 
 type Board = Grid<usize>;
-type Pos = (usize, usize);
-#[derive(EnumIter, PartialEq, Eq, Hash, Clone, Copy, Debug)]
+type Point = (usize, usize);
+#[derive(enum_iterator::Sequence, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Debug)]
 enum Dir { North, East, South, West }
-type Path = (Vec<Pos>, usize, Dir, usize);
-type Paths = Vec<Path>;
-type Weight = (cmp::Reverse<usize>, cmp::Reverse<usize>);
-
-fn weight(board: &Board, path: &Path) -> Weight {
-    (cmp::Reverse(distance(path.0.last().unwrap(), &(board.rows() - 1, board.cols() - 1))),
-     cmp::Reverse(path.3))
-}
-fn distance(p1: &Pos, p2: &Pos) -> usize {
-    ((p1.0 as isize - p2.0 as isize).abs()
-        + (p1.1 as isize - p2.1 as isize).abs()) as usize
-}
-fn show(rows: usize, cols: usize, path: &Vec<Pos>) -> Grid<char> {
-    let mut result = Grid::new(rows, cols);
-    result.fill('.');
-    for i in 1..path.len() {
-        let (rin, cin) = path[i-1];
-        let (rout, cout) = path[i];
-        *result.get_mut(rout, cout).unwrap() =
-            if rin < rout {
-                'v'
-            } else if rin > rout {
-                '^'
-            } else if cin < cout {
-                '>'
-            } else if cin > cout {
-                '<'
-            } else {
-                '?'
-            };
-    }
-    result
-}
-#[derive(PartialEq, Eq, Hash)]
-struct Key(Pos, Dir, usize);
-
-type Limits = HashMap<Key, usize>;
-fn dirs(d: Dir) -> [Dir; 3] {
-    match d {
-        Dir::North => [Dir::North, Dir::East, Dir::West],
-        Dir::East => [Dir::North, Dir::East, Dir::South],
-        Dir::South => [Dir::East, Dir::South, Dir::West],
-        Dir::West => [Dir::North, Dir::South, Dir::West]
-    }
-}
-fn read_board(name: &str) -> Board {
-    let file = fs::File::open(name).unwrap();
-    let lines = io::BufReader::new(file)
-        .lines().map(|l| l.unwrap());
-    let mut board = Board::new(0, 0);
-    for line in lines {
-        let mut row = Vec::new();
-        for c in line.chars() {
-            row.push(c.to_digit(10).unwrap() as usize);
-        }
-        board.push_row(row);
-    }
-    board
-}
-
-fn cost(board: &Board, path: &Vec<Pos>) -> usize {
-    path.iter().skip(1).fold(0, |a, (r, c)| a + board.get(*r, *c).unwrap())
-}
-
-#[test]
-fn test_optimal() {
-    let board = read_board("day17_debug.txt");
-    let opt = vec![
-        (0, 0), (0, 1), (0, 2),
-        (1, 2), (1, 3), (1, 4), (1, 5),
-        (0, 5), (0, 6), (0, 7), (0, 8),
-        (1, 8),
-        (2, 8), (2, 9), (2, 10),
-        (3, 10), (4, 10),
-        (4, 11), (5, 11), (6, 11), (7, 11),
-        (7, 12), (8, 12), (9, 12), (10, 12),
-        (10, 11), (11, 11), (12, 11), (12, 12)
-    ];
-    println!("{:#?}", show(13, 13, &opt));
-    assert_eq!(cost(&board, &opt), 102);
-}
+type Pos = (Point, Dir, usize);
+type Scores = BTreeMap<Pos, usize>;
+type Poss = BTreeSet<(usize, usize, Pos)>;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -106,62 +26,35 @@ fn main() {
     }
     let (rows, cols) = (board.rows(), board.cols());
     println!("{:#?}", board);
-    let mut paths: Paths = Paths::with_capacity(1000);
-    
-    for path in vec![
-        (vec![(0, 0), (0, 1)], 1, Dir::East, *board.get(0, 1).unwrap()),
-        (vec![(0, 0), (1, 0)], 1, Dir::South, *board.get(1, 0).unwrap())
-    ] {
-        let i = match paths.binary_search_by_key(&weight(&board, &path), |p| { weight(&board, p) }) {
-            Err(i) => i,
-            Ok(i) => i + 1
-        };
-        paths.insert(i, path);
-    }
-    let mut limits = Limits::new();
-    limits.insert(Key((0, 1), Dir::East, 2), *board.get(0, 1).unwrap());
-    limits.insert(Key((1, 0), Dir::East, 2), *board.get(1, 0).unwrap());
+    let mut scores = Scores::new();
+    let mut poss = Poss::new();
+    poss.insert(((rows - 1) + (cols - 1), 0, ((0, 0), Dir::East, 3)));
     let mut limit = usize::MAX;
-    let mut k = 0;
-    while let Some((path, rest, din, cost)) = paths.pop() {
-        k += 1;
-        if k % 1_000_000 == 0 {
-            println!("{} {} {}", k, limit, paths.len());
-            // println!("{:?} {} {:?}, {}", path.clone(), len, din, w);
-        }
-        let &pos = path.last().unwrap();
-        if pos == (rows - 1, cols - 1) {
-            limit = limit.min(cost);
+    while let Some((dist, score, pos)) = poss.pop_first() {
+        if dist == 0 {
+            limit = limit.min(score);
             println!("{}", limit);
-            // println!("{:#?}", show(rows, cols, &path));
             continue;
         }
-        for dout in dirs(din) {
-            if rest == 0 && dout == din { continue; }
-            let (mut r, mut c) = pos;
-            match dout {
-                Dir::North => { r = if r == 0 { continue; } else { r - 1 }; },
-                Dir::East => { c = if c == cols - 1 { continue; } else { c + 1 }; },
-                Dir::South => { r = if r == rows - 1 { continue; } else { r + 1 }; },
-                Dir::West => { c = if c == 0 { continue; } else { c - 1 }; }
+        if score >= limit { continue; }
+        if score < *scores.get(&pos).unwrap_or(&usize::MAX) {
+            scores.insert(pos, score);
+        } else {
+            continue;
+        }
+        let (point, dir, count) = pos;
+        for next_dir in enum_iterator::all::<Dir>() {
+            if count == 0 && next_dir == dir { continue; }
+            let point = match dir {
+                Dir::North => if point.0 == 0 { continue } else { (point.0 - 1, point.1) },
+                Dir::East => if point.1 == cols - 1 { continue } else { (point.0, point.1 + 1) },
+                Dir::South => if point.0 == rows - 1 { continue } else { (point.0 + 1, point.1) },
+                Dir::West => if point.1 == 0 { continue } else { (point.0, point.1 - 1) }
             };
-            if cost + board.get(r, c).unwrap() >= limit { continue; }
-            let rest = if dout == din { rest - 1 } else { 2 };
-            let key = Key((r, c), dout, rest);
-            if cost + board.get(r, c).unwrap() >= *limits.get(&key).unwrap_or(&usize::MAX) {
-                continue;
-            } else {
-                limits.insert(key, cost + board.get(r, c).unwrap());
-            }
-            // if path.contains(&(r, c)) { continue; }
-            let mut path = path.clone();
-            path.push((r, c));
-            let path = (path, rest, dout, cost + board.get(r, c).unwrap());
-            let i = match paths.binary_search_by_key(&weight(&board, &path), |p| weight(&board, p)) {
-                Err(i) => i,
-                Ok(i) => i + 1
-            };
-            paths.insert(i, path);
+            let count = if next_dir == dir { count - 1 } else { 3 };
+            let dist = (rows - 1 - point.0) + (cols - 1 - point.1);
+            let score = score + board.get(point.0, point.1).unwrap();
+            poss.insert((dist, score, (point, next_dir, count)));
         }
     }
     let result = limit;
---
+++ src/bin/day18_1.rs
@@ -0,0 +1,72 @@
+use std::{fs, env, io::{self, BufRead}};
+use grid::Grid;
+
+type Board = Grid<char>;
+fn is_internal(right: isize, (row, col): &(isize, isize), circuit: &Vec<(isize, isize)>) -> bool {
+    let mut crosses_count = 0;
+    let mut i = col + 1;
+    while i <= right {
+        if !circuit.contains(&(*row, i)) {
+            i += 1;
+            continue;
+        }
+        let mut j = i + 1;
+        while j <= right && circuit.contains(&(*row, j)) { j += 1; }
+        if !circuit.contains(&(*row, j)) { j -= 1; }
+        if circuit.contains(&(row - 1, i)) && circuit.contains(&(row + 1, j)) ||
+            circuit.contains(&(row + 1, i)) && circuit.contains(&(row - 1, j)) {
+                crosses_count += 1;
+            }
+        i = j + 1;
+        i += 1;
+    }
+    crosses_count % 2 == 1
+}
+
+fn main() {
+    let mut result = 0;
+    let args: Vec<String> = env::args().collect();
+    let file = fs::File::open(&args[1]).unwrap();
+    let lines = io::BufReader::new(file)
+        .lines().map(|l| l.unwrap());
+    let (mut row, mut col) = (0, 0);
+    let mut circuit = vec![];
+    for line in lines {
+        let mut items = line.split_whitespace();
+        let dir = items.next().unwrap().chars().next().unwrap();
+        let dist = items.next().unwrap().parse::<usize>().unwrap();
+        for _ in 0..dist {
+            match dir {
+                'U' => { row -= 1; },
+                'R' => { col += 1; },
+                'D' => { row += 1; },
+                'L' => { col -= 1; },
+                _ => panic!("Invalid dir")
+            };
+            circuit.push((row, col));
+        }
+    }
+    let top = circuit.iter().map(|p| p.0).min().unwrap();
+    let bottom = circuit.iter().map(|p| p.0).max().unwrap();
+    let left = circuit.iter().map(|p| p.1).min().unwrap();
+    let right = circuit.iter().map(|p| p.1).max().unwrap();
+    let mut board = Board::init((bottom - top + 1) as usize, (right - left + 1) as usize, '.');
+    for (row, col) in &circuit {
+        *board.get_mut(row - top, col - left).unwrap() = '#';
+    }
+    for row in (top+1)..bottom {
+        eprintln!("Row {}", row);
+        for col in (left+1)..right {
+            if !circuit.contains(&(row, col)) {
+                if is_internal(right, &(row, col), &circuit) {
+                    result += 1;
+                    *board.get_mut(row - top, col - left).unwrap() = '#';
+                } else {
+                    //*board.get_mut(row - top, col - left).unwrap() = '0';
+                }
+            }
+        }
+    }
+    result += circuit.len();
+    eprintln!("{:?}", result);
+}
